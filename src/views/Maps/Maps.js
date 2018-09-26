e357037a81d7ef4e1f0ff130a824348a46a9f271
mally13 <mallymutuku13@gmail.com>
2018-09-26 22:56:32 +0300

    minor changes....

--- src/views/Maps/Maps.js
+++ src/views/Maps/Maps.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import { Row, Col, Card, CardHeader, CardBody } from "reactstrap";
-import Geocode from "react-geocode";
+// import Geocode from "react-geocode";
 // react plugin used to create google maps
 import {
   withScriptjs,
@@ -20,109 +20,46 @@ const options = require('variables/counties_of_Kenya.json');
 const baseURL = "http://api.kmhfltest.health.go.ke";
 const FacilityEndPoint = "facilities/facilities";
 const MapWrapper = withScriptjs(
-    withGoogleMap(props => (
-      <GoogleMap
-        defaultZoom={13}
-        defaultCenter={{ lat: 0.748817, lng: -33.985428 }}
-        defaultOptions={{
-          scrollwheel: false,
-          styles: [
-            {
-              featureType: "water",
-              elementType: "geometry",
-              stylers: [{ color: "#e9e9e9" }, { lightness: 17 }]
-            },
-            {
-              featureType: "landscape",
-              elementType: "geometry",
-              stylers: [{ color: "#f5f5f5" }, { lightness: 20 }]
-            },
-            {
-              featureType: "road.highway",
-              elementType: "geometry.fill",
-              stylers: [{ color: "#ffffff" }, { lightness: 17 }]
-            },
-            {
-              featureType: "road.highway",
-              elementType: "geometry.stroke",
-              stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }]
-            },
-            {
-              featureType: "road.arterial",
-              elementType: "geometry",
-              stylers: [{ color: "#ffffff" }, { lightness: 18 }]
-            },
-            {
-              featureType: "road.local",
-              elementType: "geometry",
-              stylers: [{ color: "#ffffff" }, { lightness: 16 }]
-            },
-            {
-              featureType: "poi",
-              elementType: "geometry",
-              stylers: [{ color: "#f5f5f5" }, { lightness: 21 }]
-            },
-            {
-              featureType: "poi.park",
-              elementType: "geometry",
-              stylers: [{ color: "#dedede" }, { lightness: 21 }]
-            },
-            {
-              elementType: "labels.text.stroke",
-              stylers: [
-                { visibility: "on" },
-                { color: "#ffffff" },
-                { lightness: 16 }
-              ]
-            },
-            {
-              elementType: "labels.text.fill",
-              stylers: [
-                { saturation: 36 },
-                { color: "#333333" },
-                { lightness: 40 }
-              ]
-            },
-            { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
-            {
-              featureType: "transit",
-              elementType: "geometry",
-              stylers: [{ color: "#f2f2f2" }, { lightness: 19 }]
-            },
-            {
-              featureType: "administrative",
-              elementType: "geometry.fill",
-              stylers: [{ color: "#fefefe" }, { lightness: 20 }]
-            },
-            {
-              featureType: "administrative",
-              elementType: "geometry.stroke",
-              stylers: [{ color: "#fefefe" }, { lightness: 17 }, { weight: 1.2 }]
-            }
-          ]
-        }}
-      >
-        <Marker position={{ lat: Facilities.lat, lng: Facilities.long }} >
-       <infoWindow>
-           <div>
-               {Facilities.id}
-           </div>
-       </infoWindow>
-       }
-       </Marker>
-      )
-    })}
-      </GoogleMap>
-    ))
-);
-
-class Maps extends React.Component {
+  withGoogleMap(props => {
+    return(
+    <GoogleMap
+      ref={props.onMapLoad}
+      defaultZoom={13}
+      defaultCenter={{ lat: 0.748817, lng: -33.985428 }}
+      defaultOptions={{
+        scrollwheel: false,
+      styles:fancy}}
+    >
+      {props.markers.map((marker,index)=>
+    <Marker
+    position={marker.position}
+    onClick={()=> props.onMarkerClick(marker)}
+    >
+    {marker.showInfo &&(
+     <infoWindow onCloseClick={()=> props.onMarkerClose(marker)}>
+     {
+       marker.content
+     } 
+     </infoWindow>
+     ) }
+     </Marker>
+  )}}   
+   </GoogleMap>
+  )
+class FullScreenMap extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            facilities: []
+            markers: [],
+            formValue:""
         }
+        this.handleMapClick=this.handleMapClick.bind(this);
+        this.handleMarkerClick=this.handleMarkerClick.bind(this);
+        this.handleMarkerClose=this.handleMarkerClose.bind(this);
+        this.handleChange=this.handleChange.bind(this);  
+        this.UpdatingContent=this.UpdatingContent.bind(this);      
+
     }
 
     componentDidMount() {
@@ -130,8 +67,54 @@ class Maps extends React.Component {
       axios.get(`${baseURL}/api/${FacilityEndPoint}/?fields=id,lat_long&format=json&page_size=5`, {
           headers:
               { Authorization: `Bearer ${token.accessToken}` }
-      }).then((response) => {
-          console.log("success",response.data.results)
+      }).then(response=> {
+        const markerData= response.data;
+        console.log(markerData)
+        const nextMarkers=markerData.map(markerObject =>{
+          const latlng={lat:Number(markerData.Object.lat_long[0]),lat:Number(markerData.Object.lat_long[1])}
+          /*const content=markerObject.content ? markerObject.content: null*/
+          return{
+            id:markerObject.id,
+            position:latlng,
+           /* content:content */
+          }
+        })
+        this.setState({
+          markers:nextMarkers
+        })
+      })
+    }
+    handleChange(event){
+      this.setState({formValue:event.target.value})
+    }
+
+    handleMarkerRightClick(targetMarker){
+      const nextMarkers=this.state.markers.filter(marker => marker !==targetMarker);
+      this.setState({
+        markers:nextMarkers,
+      }); 
+    }
+    handleMarkerClick(targetMarker){
+      this.setState({
+        markers:this.state.markers.map(marker =>{
+          if (marker===targetMarker) marker.showInfo= true
+            console.log(marker)
+            return marker;
+           }),
+        })
+       }
+
+    handleMarkerClose(targetMarker){
+      this.setState({
+        markers:this.state.markers.map(marker =>{
+          if(marker===targetMarker) marker.showInfo=false
+          return marker;
+        }),
+      })
+    }
+  
+        /*
+         { console.log("success",response.data.results)
           var Facilities = response.data.results.map( function(res){
            return({
                 id: res.id,
@@ -140,7 +123,7 @@ class Maps extends React.Component {
             
           })});
           console.log("facilities",Facilities);
-          this.setState({ Facilities});
+          this.setState({ facilities:Facilities[0]});
       })
           .catch((error) => {
               console.log(error);
@@ -148,10 +131,10 @@ class Maps extends React.Component {
 }
 handleClick= (marker, event)=>{
     this.setState({selectedMarker:marker})
-}
+}}*/
 
 render() {
-            return (
+   return (
                 <div>
                 <PanelHeader size="sm" />
                 <div className="content">
@@ -166,22 +149,101 @@ render() {
                             style={{ position: "relative", overflow: "hidden" }}
                           >
                             <MapWrapper
-                              selectedMarker={this.state.selectedMarker}
-                              onClick={this.handleClick}
-
-                              googleMapURL="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"
+                              markers={this.state.markers}
+                              onMapLoad={this.handleMapLoad}
+                              onMapClick={this.handleMapClick}
+                              onMarkerRightClick={this.handleMarkerRightClick}
+                              onMarkerClick={this.handleMarkerClick}
+                              onMarkerClose={this.handleMarkerClose}
+                              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAh_B-XTyhwOalsCZIqWnagWt9teigisio"
                               loadingElement={<div style={{ height: `100%` }} />}
                               containerElement={<div style={{ height: `100%` }} />}
                               mapElement={<div style={{ height: `100%` }} />}
                             />
-                          </div>
+                          </div> 
                         </CardBody>
                       </Card>
                     </Col>
                   </Row>
                 </div>
               </div> 
-            )
-            }}
+            );
+            }
+          }
+  }))
+  export default FullScreenMap;
+  const fancy=[
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#e9e9e9" }, { lightness: 17 }]
+    },
+    {
+      featureType: "landscape",
+      elementType: "geometry",
+      stylers: [{ color: "#f5f5f5" }, { lightness: 20 }]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.fill",
+      stylers: [{ color: "#ffffff" }, { lightness: 17 }]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }]
+    },
+    {
+      featureType: "road.arterial",
+      elementType: "geometry",
+      stylers: [{ color: "#ffffff" }, { lightness: 18 }]
+    },
+    {
+      featureType: "road.local",
+      elementType: "geometry",
+      stylers: [{ color: "#ffffff" }, { lightness: 16 }]
+    },
+    {
+      featureType: "poi",
+      elementType: "geometry",
+      stylers: [{ color: "#f5f5f5" }, { lightness: 21 }]
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry",
+      stylers: [{ color: "#dedede" }, { lightness: 21 }]
+    },
+    {
+      elementType: "labels.text.stroke",
+      stylers: [
+        { visibility: "on" },
+        { color: "#ffffff" },
+        { lightness: 16 }
+      ]
+    },
+    {
+      elementType: "labels.text.fill",
+      stylers: [
+        { saturation: 36 },
+        { color: "#333333" },
+        { lightness: 40 }
+      ]
+    },
+    { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
+    {
+      featureType: "transit",
+      elementType: "geometry",
+      stylers: [{ color: "#f2f2f2" }, { lightness: 19 }]
+    },
+    {
+      featureType: "administrative",
+      elementType: "geometry.fill",
+      stylers: [{ color: "#fefefe" }, { lightness: 20 }]
+    },
+    {
+      featureType: "administrative",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#fefefe" }, { lightness: 17 }, { weight: 1.2 }]
+    }
 
-export default Maps;
+  ]
--- src/views/Maps/Maps.jsx
+++ src/views/Maps/Maps.jsx
@@ -1,134 +1,255 @@
-import React,{ Component } from "react";
-import ScriptTag from 'react-script-tag';
-import Maps from "./Maps.js"
+import React from "react";
+import axios from "axios";
+import {default as Component} from "react";
 import { Row, Col, Card, CardHeader, CardBody } from "reactstrap";
-import { PanelHeader } from "components";
-import script from "./Maps.js"
+// import Geocode from "react-geocode";
+// react plugin used to create google maps
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 
+import { PanelHeader } from "components";
+var map;
+var geocoder;
+
+// const fetchData = require("../../api");
+const token = require('variables/keys.json');
+const options = require('variables/counties_of_Kenya.json');
+const baseURL = "http://api.kmhfltest.health.go.ke";
+const FacilityEndPoint = "facilities/facilities";
 const MapWrapper = withScriptjs(
   withGoogleMap(props => (
-    <GoogleMap
+      <GoogleMap
+      ref={props.onMapLoad}
       defaultZoom={13}
       defaultCenter={{ lat: 0.748817, lng: -33.985428 }}
       defaultOptions={{
         scrollwheel: false,
-        styles: [
-          {
-            featureType: "water",
-            elementType: "geometry",
-            stylers: [{ color: "#e9e9e9" }, { lightness: 17 }]
-          },
-          {
-            featureType: "landscape",
-            elementType: "geometry",
-            stylers: [{ color: "#f5f5f5" }, { lightness: 20 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#ffffff" }, { lightness: 17 }]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }]
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "geometry",
-            stylers: [{ color: "#ffffff" }, { lightness: 18 }]
-          },
-          {
-            featureType: "road.local",
-            elementType: "geometry",
-            stylers: [{ color: "#ffffff" }, { lightness: 16 }]
-          },
-          {
-            featureType: "poi",
-            elementType: "geometry",
-            stylers: [{ color: "#f5f5f5" }, { lightness: 21 }]
-          },
-          {
-            featureType: "poi.park",
-            elementType: "geometry",
-            stylers: [{ color: "#dedede" }, { lightness: 21 }]
-          },
-          {
-            elementType: "labels.text.stroke",
-            stylers: [
-              { visibility: "on" },
-              { color: "#ffffff" },
-              { lightness: 16 }
-            ]
-          },
-          {
-            elementType: "labels.text.fill",
-            stylers: [
-              { saturation: 36 },
-              { color: "#333333" },
-              { lightness: 40 }
-            ]
-          },
-          { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
-          {
-            featureType: "transit",
-            elementType: "geometry",
-            stylers: [{ color: "#f2f2f2" }, { lightness: 19 }]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.fill",
-            stylers: [{ color: "#fefefe" }, { lightness: 20 }]
-          },
-          {
-            featureType: "administrative",
-            elementType: "geometry.stroke",
-            stylers: [{ color: "#fefefe" }, { lightness: 17 }, { weight: 1.2 }]
-          }
-        ]
-      }}
+      styles:fancy}}
     >
-      <Marker position={{ lat: Facilities.lat, lng: Facilities.long }} >
-     <infoWindow>
-         <div>
-             {Facilities.id}
-         </div>
+      {props.markers.map((marker,index)=>
+    <Marker
+    position={marker.position}
+    onClick={()=> props.onMarkerClick(marker)}
+    >
+    {marker.showInfo &&(
+     <infoWindow onCloseClick={()=> props.onMarkerClose(marker)}>
+     {
+       marker.content
+     } 
      </infoWindow>
-     }
+     ) }
      </Marker>
-    )
-  })}
-    </GoogleMap>
+  )}}   
+   </GoogleMap>
   ))
 );
+export default class FullScreenMap extends React.Component {
+    constructor(props) {
+        super(props); 
 
-class FullScreenMap extends React.Component {
-  render() {
-    return (
-      <div>
-        <PanelHeader size="sm" />
-        <div className="content">
-          <Row>
-            <Col xs={12}>
-              <Card>
-                <CardHeader>Google Map</CardHeader>
-                <CardBody>
-                  <div>
-                      <MapWrapper id="Map"
-                      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfVlHaotgmh79R-JgDCO8zGXfp2sigANQ&callback=loadMap"
-                      loadingElement={<div style={{ height: `100%` }} />}
-                      containerElement={<div style={{ height: `100%` }} />}
-                      mapElement={<div style={{ height: `100%` }} />}
-                    />
-                    </div>     
-       
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </div>
-      </div>
-    );
-  }
+        this.state = {
+            markers: [],
+            formValue:"",
+            isReady:false,  //create isReady on the state
+        }
+       /* this.handleMapClick=this.handleMapClick.bind(this);*/
+        this.handleMarkerClick=this.handleMarkerClick.bind(this);
+        this.handleMarkerClose=this.handleMarkerClose.bind(this);
+        this.handleChange=this.handleChange.bind(this);  
+             
+
+    }
+
+    componentDidMount() {
+      //get  facilities options
+      axios.get(`${baseURL}/api/${FacilityEndPoint}/?fields=id,lat_long&format=json&page_size=5`, {
+          headers:
+              { Authorization: `Bearer ${token.accessToken}` }
+      }).then(response=> {
+        const markerData= response.data.results;
+        console.log("results",response.data)
+        
+        const nextMarkers= markerData.map(markerData=>{
+          const latlng={lat:Number(markerData.lat_long[0]),lng:Number(markerData.lat_long[1])}
+          return{
+            id:markerData.id,
+            position:latlng
+          }
+        })
+        this.setState({
+          markers:nextMarkers
+        })
+
+        })
+      }
+     
+
+    handleChange(event){
+      this.setState({formValue:event.target.value})
+    }
+
+    handleMarkerRightClick(targetMarker){
+      const nextMarkers=this.state.markers.filter(marker => marker !==targetMarker);
+      this.setState({
+        markers:nextMarkers,
+      }); 
+    }
+    handleMarkerClick(targetMarker){
+      this.setState({
+        markers:this.state.markers.map(marker =>{
+          if (marker===targetMarker) marker.showInfo= true
+            console.log(marker)
+            return marker;
+           }),
+        })
+       }
+
+    handleMarkerClose(targetMarker){
+      this.setState({
+        markers:this.state.markers.map(marker =>{
+          if(marker===targetMarker) marker.showInfo=false
+          return marker;
+        }),
+      })
+    }
+  
+        /*
+         { console.log("success",response.data.results)
+          var Facilities = response.data.results.map( function(res){
+           return({
+                id: res.id,
+                lat:res.lat_long[0],
+                long: res.lat_long[1]
+            
+          })});
+          console.log("facilities",Facilities);
+          this.setState({ facilities:Facilities[0]});
+      })
+          .catch((error) => {
+              console.log(error);
+          })
 }
+handleClick= (marker, event)=>{
+    this.setState({selectedMarker:marker})
+}}*/
 
+render() {
+ /*const {isReady}=this.state;
+  if(isReady)*/
+   return (
+                <div>
+                <PanelHeader size="sm" />
+                <div className="content">
+                  <Row>
+                    <Col xs={12}>
+                      <Card>
+                        <CardHeader>Google Map</CardHeader>
+                        <CardBody>
+                          <div
+                            id="map"
+                            className="map"
+                            style={{ position: "relative", overflow: "hidden" }}
+                          >
+                            <MapWrapper
+                              markers={this.state.markers}
+                              onMapLoad={this.handleMapLoad}
+                              onMapClick={this.handleMapClick}
+                              onMarkerRightClick={this.handleMarkerRightClick}
+                              onMarkerClick={this.handleMarkerClick}
+                              onMarkerClose={this.handleMarkerClose}
+                              googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAh_B-XTyhwOalsCZIqWnagWt9teigisio"
+                              loadingElement={<div style={{ height: `100%` }} />}
+                              containerElement={<div style={{ height: `100%` }} />}
+                              mapElement={<div style={{ height: `100%` }} />}
+                            />
+                          </div> 
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  </Row>
+                </div>
+              </div> 
+            );
+            }
+          }
+          const fancy=[
+            {
+              featureType: "water",
+              elementType: "geometry",
+              stylers: [{ color: "#e9e9e9" }, { lightness: 17 }]
+            },
+            {
+              featureType: "landscape",
+              elementType: "geometry",
+              stylers: [{ color: "#f5f5f5" }, { lightness: 20 }]
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry.fill",
+              stylers: [{ color: "#ffffff" }, { lightness: 17 }]
+            },
+            {
+              featureType: "road.highway",
+              elementType: "geometry.stroke",
+              stylers: [{ color: "#ffffff" }, { lightness: 29 }, { weight: 0.2 }]
+            },
+            {
+              featureType: "road.arterial",
+              elementType: "geometry",
+              stylers: [{ color: "#ffffff" }, { lightness: 18 }]
+            },
+            {
+              featureType: "road.local",
+              elementType: "geometry",
+              stylers: [{ color: "#ffffff" }, { lightness: 16 }]
+            },
+            {
+              featureType: "poi",
+              elementType: "geometry",
+              stylers: [{ color: "#f5f5f5" }, { lightness: 21 }]
+            },
+            {
+              featureType: "poi.park",
+              elementType: "geometry",
+              stylers: [{ color: "#dedede" }, { lightness: 21 }]
+            },
+            {
+              elementType: "labels.text.stroke",
+              stylers: [
+                { visibility: "on" },
+                { color: "#ffffff" },
+                { lightness: 16 }
+              ]
+            },
+            {
+              elementType: "labels.text.fill",
+              stylers: [
+                { saturation: 36 },
+                { color: "#333333" },
+                { lightness: 40 }
+              ]
+            },
+            { elementType: "labels.icon", stylers: [{ visibility: "off" }] },
+            {
+              featureType: "transit",
+              elementType: "geometry",
+              stylers: [{ color: "#f2f2f2" }, { lightness: 19 }]
+            },
+            {
+              featureType: "administrative",
+              elementType: "geometry.fill",
+              stylers: [{ color: "#fefefe" }, { lightness: 20 }]
+            },
+            {
+              featureType: "administrative",
+              elementType: "geometry.stroke",
+              stylers: [{ color: "#fefefe" }, { lightness: 17 }, { weight: 1.2 }]
+            }
+        
+          ]
+        
+  
